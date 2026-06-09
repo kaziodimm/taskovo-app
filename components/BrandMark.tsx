8d18bd57c89b0a47b0d0c044a226767085617f03
kaziodimm <kaziodimm@gmail.com>
2026-06-09 10:28:01 +0200

    Use Taskovo logo asset in brand mark

--- components/BrandMark.tsx
+++ components/BrandMark.tsx
@@ -1,13 +1,3 @@
 export function BrandMark({ large = false }: { large?: boolean }) {
-  return (
-    <span className={large ? "brand-mark brand-mark-large" : "brand-mark"} aria-hidden="true">
-      <span className="brand-skyline" />
-      <span className="brand-building brand-building-left" />
-      <span className="brand-building brand-building-right" />
-      <span className="brand-road-left" />
-      <span className="brand-road-right" />
-      <span className="brand-road-line" />
-      <span className="brand-person" />
-    </span>
-  );
+  return <img className={large ? "brand-mark brand-mark-large" : "brand-mark"} src="/taskovo-logo.svg" alt="" aria-hidden="true" />;
 }
